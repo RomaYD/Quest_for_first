b74aaea3007d9bc25c93c702a45a14cb3c5d39b0
F4r3t <sultangareevalan@gmail.com>
2024-10-01 21:03:41 +0300

    Update code and change params stations

--- stations.py
+++ stations.py
@@ -1,24 +1,24 @@
 import json
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from Base import Group, Base, Station, User, Settings
+from Base import Group, Station, User
 
 
-def return_classic_stats(session, roma, gelia):
+def return_classic_stats(session, roma):
     with open('db.json', 'r', encoding='UTF-8') as file:
         stations_json = json.load(file)
         for i in stations_json['stations']:
             stations = Station(id=i['id'], name=i['name'], geo=i['geo'], reward=i['reward'], group=i['group'])
             session.add(stations)
             session.commit()
-    for i in range(101, 118):
+    for i in range(101, 117):
         if i != 110:
             ed_group = Group(id=i, stations='', current_station=0, experience=0, money=0)
             session.add(ed_group)
             session.commit()
     roma_add = User(id=1311111008, username='borxgalki', full_name='Roman', station = roma.station, type=2)
-    gelia_add = User(id=1928032533, username='babagelya', full_name='ГАВ', station = gelia.station, type=2)
+    # gelia_add = User(id=1928032533, username='babagelya', full_name='ГАВ', station = gelia.station, type=2)
+    alan_add = User(id=1185896077, username='f4r3t', full_name='Alan Sultangareev', station = None, type=2)
     session.add(roma_add)
-    session.add(gelia_add)
+    # session.add(gelia_add)
+    session.add(alan_add)
     session.commit()
 
